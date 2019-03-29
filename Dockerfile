1f225582ca0b512309a8c3d101010ab2c28ea6d1
Jamie Markle <jrmarkle@users.noreply.github.com>
2019-03-29 14:11:15 -0400

    testing memory usage

--- Dockerfile
+++ Dockerfile
@@ -6,29 +6,15 @@ ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update
 RUN apt-get install -y \
-	apt-transport-https \
-	apt-utils \
-	ca-certificates \
-	curl \
 	git \
-	software-properties-common \
 	build-essential \
 	libboost-program-options-dev \
 	libboost-system-dev \
 	libboost-test-dev \
 	libboost-thread-dev \
 	zlib1g-dev \
-	netcat \
-	python \
-	default-jdk \
-	libtool \
-	m4 \
-	autoconf \
-	automake \
 	valgrind \
-	pkg-config \
-	cmake \
-	help2man
+	cmake
 
 RUN git clone git://github.com/VowpalWabbit/vowpal_wabbit.git /opt/vowpal_wabbit/
 WORKDIR /opt/vowpal_wabbit
@@ -37,20 +23,20 @@ RUN git -c advice.detachedHead=false checkout edfc0ba8e0c792c65e7732a8cbe03b0de4
 
 COPY revert-e833199e.patch .
 RUN patch -p1 < revert-e833199e.patch
+COPY close_file_fix.patch .
+RUN patch -p1 < close_file_fix.patch
 
 RUN mkdir build
 WORKDIR /opt/vowpal_wabbit/build
-RUN cmake .. -DSTATIC_LINK_VW=On -DCMAKE_BUILD_TYPE=Release
+RUN cmake ..
 RUN make -j$(cat nprocs.txt) install
 RUN ldconfig
 
 WORKDIR /root
 
-COPY main.c /root/main.c
+COPY main.c .
 
 RUN gcc -c main.c -I/usr/local/include/vowpalwabbit
-RUN g++ -static main.o -o test -lvw_c_wrapper -lvw -lallreduce -pthread -lboost_program_options -lz
+RUN g++ main.o -o test -lvw_c_wrapper -lvw -lallreduce -pthread -lboost_program_options -lz
 
-RUN file test
-
-CMD ["./test"]
+CMD ["valgrind", "./test"]
--- main.c
+++ main.c
@@ -26,59 +26,51 @@ int main() {
 			}
 		}
 
-		{
-			VW_EXAMPLE goodExample = VW_ReadExampleA(h, "|Feature Good");
-			float prediction = VW_Learn(h, goodExample);
-			float confidence = VW_GetConfidence(goodExample);
-			printf("predict good = %f, confidence = %f\n", prediction, confidence);
-			VW_FinishExample(h, goodExample);
-		}
-		{
-			VW_EXAMPLE goodExample = VW_ReadExampleA(h, "|Feature Bad");
-			float prediction = VW_Learn(h, goodExample);
-			float confidence = VW_GetConfidence(goodExample);
-			printf("predict bad = %f, confidence = %f\n", prediction, confidence);
-			VW_FinishExample(h, goodExample);
-		}
-		{
-			VW_EXAMPLE goodExample = VW_ReadExampleA(h, "|Feature Unknown");
-			float prediction = VW_Learn(h, goodExample);
-			float confidence = VW_GetConfidence(goodExample);
-			printf("predict unknown = %f, confidence = %f\n", prediction, confidence);
-			VW_FinishExample(h, goodExample);
-		}
-
 		VW_CopyModelData(h, &buf, &modelBuf, &modelBufLen);
 		VW_Finish(h);
 	}
 
-	printf("-------\n");
-
-	{
+	for (int i = 0; i < 1e1; i++) {
+		printf("%d...\n", i);
 		VW_HANDLE h = VW_InitializeWithModel(commandLine, modelBuf, modelBufLen);
-
-		{
-			VW_EXAMPLE goodExample = VW_ReadExampleA(h, "|Feature Good");
-			float prediction = VW_Learn(h, goodExample);
-			float confidence = VW_GetConfidence(goodExample);
-			printf("predict good = %f, confidence = %f\n", prediction, confidence);
-			VW_FinishExample(h, goodExample);
+		for (int j = 0; j < 1e6; j++) {
+			{
+				VW_EXAMPLE goodExample = VW_ReadExampleA(h, "|Feature Good");
+				float prediction = VW_Learn(h, goodExample);
+				float confidence = VW_GetConfidence(goodExample);
+				if (prediction < 0.9 || confidence < 10) {
+					printf("predict good = %f, confidence = %f\n", prediction, confidence);
+					VW_FinishExample(h, goodExample);
+					VW_Finish(h);
+					return 1;
+				}
+				VW_FinishExample(h, goodExample);
+			}
+			{
+				VW_EXAMPLE badExample = VW_ReadExampleA(h, "|Feature Bad");
+				float prediction = VW_Learn(h, badExample);
+				float confidence = VW_GetConfidence(badExample);
+				if (prediction > 0.1 || confidence < 10) {
+					printf("predict bad = %f, confidence = %f\n", prediction, confidence);
+					VW_FinishExample(h, badExample);
+					VW_Finish(h);
+					return 1;
+				}
+				VW_FinishExample(h, badExample);
+			}
+			{
+				VW_EXAMPLE unknownExample = VW_ReadExampleA(h, "|Feature Unknown");
+				float prediction = VW_Learn(h, unknownExample);
+				float confidence = VW_GetConfidence(unknownExample);
+				if (prediction > 0.6 || prediction < 0.4 || confidence > 0) {
+					printf("predict unknown = %f, confidence = %f\n", prediction, confidence);
+					VW_FinishExample(h, unknownExample);
+					VW_Finish(h);
+					return 1;
+				}
+				VW_FinishExample(h, unknownExample);
+			}
 		}
-		{
-			VW_EXAMPLE goodExample = VW_ReadExampleA(h, "|Feature Bad");
-			float prediction = VW_Learn(h, goodExample);
-			float confidence = VW_GetConfidence(goodExample);
-			printf("predict bad = %f, confidence = %f\n", prediction, confidence);
-			VW_FinishExample(h, goodExample);
-		}
-		{
-			VW_EXAMPLE goodExample = VW_ReadExampleA(h, "|Feature Unknown");
-			float prediction = VW_Learn(h, goodExample);
-			float confidence = VW_GetConfidence(goodExample);
-			printf("predict unknown = %f, confidence = %f\n", prediction, confidence);
-			VW_FinishExample(h, goodExample);
-		}
-
 		VW_Finish(h);
 	}
 
